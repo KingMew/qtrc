367ffbba42c04ecc7260dba7c4b6e724195c6ee2
Mew <Giratina493@mew151.net>
2014-11-16 16:47:25 -0700

    See changelog in README

---
+++ global.h
@@ -0,0 +1,18 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+//#define DEBUG
+
+enum RC_POSITION_MODE {
+    RC_CURSOR,
+    RC_RANDOM,
+    RC_CENTER
+};
+
+/*TODO:
+ * Save configuration in a config file
+ * Make phasetimer configurable (?maybe base it on the user's past usage?)
+ * Make UI a bit prettier?
+*/
+
+#endif // GLOBAL_H
